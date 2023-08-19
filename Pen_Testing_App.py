abb334497976148068e782ceb76a019fd1b1c750
Mullas1034 <77933107+Mullas1034@users.noreply.github.com>
2023-08-20 00:00:26 +0100

    Update Pen_Testing_App.py

--- Pen_Testing_App.py
+++ Pen_Testing_App.py
@@ -90,7 +90,6 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 	
 def main_menu():
-	def Prevent_Sleep():
 	while True:
 		os.system("xset s off")
 		os.system("xset -dpms")
