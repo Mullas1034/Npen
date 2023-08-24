5efb0a027c01076d85c037969244eba5a211d8a4
Mullas1034 <77933107+Mullas1034@users.noreply.github.com>
2023-08-25 00:22:41 +0100

    Update nmap_scanner.py

--- nmap_scanner.py
+++ nmap_scanner.py
@@ -164,11 +164,7 @@ def SinglePortScan(IPs):
 		print('[--------------------------SCAN-----------------------------]')
 		if type(IPs) == list:
 			processes = [Process(target=portscan, args=(IPaddrs[i],))for i in IPs]
-			if len(IPs) == 1 and len(processes) != 1:
-				processes.pop()
-			last_proc = processes[-1]
 			for process in processes:
-				print(process)
 				process.start()
 				proc.append(process)
 				run_max += 1
